214c14cf0dc9451289eaf1f7349be34fbe9a1f40
abuttle2 <alexbuttle@hotmail.co.uk>
2023-01-22 15:14:55 +0000

    Added a function to get the current hour and loop through time blocks

--- starter/logic.js
+++ starter/logic.js
@@ -1,11 +1,25 @@
 // handle displaying the time
 let timeDisplayEl = $("#currentDay");
+let descriptionsEls = $(".description");
 
 let displayTime = function () {
     var currentDateTime = moment().format('dddd, MMMM Do [at] hh:mm:ss a');
     timeDisplayEl.text(currentDateTime);
 }
 
-// dddd, MMMM Do
-
 setInterval(displayTime, 1000);
+
+let setCellColor = function () {
+    const currentTime = moment().format("ha");
+    const timeBlockEls = $(".time-block");
+    console.log(currentTime);
+
+    //Iterate through each of the time blocks to check the current hour
+    for (var i = 0; i < timeBlockEls.length; i++) {
+        console.log(timeBlockEls[i]);
+    }
+
+
+}
+
+setCellColor();
