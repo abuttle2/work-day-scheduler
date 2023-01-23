3b72fe5b11da3b8bed3c572c58ae2d001aa8e6f7
abuttle2 <alexbuttle@hotmail.co.uk>
2023-01-23 21:18:22 +0000

    Added colours to past, present & future hours dynamically

--- starter/logic.js
+++ starter/logic.js
@@ -9,89 +9,18 @@ let displayTime = function () {
 
 setInterval(displayTime, 1000);
 
+// const currentHour = moment().format("ha");
+
 //Loop through the dates in HTML and store in an array
 let pushDates = function () {
-    const testTime = moment("10am", "ha").format("ha");
-    const currentHour = moment().format("ha");
     const timeBlockEls = $(".time-block");
-    var hoursArray = []
+    var hoursArray = [];
 
-
-    //Iterate through each time block
+    //Iterate through each time block stores hours from html into an array
     for (var i = 0; i < timeBlockEls.length; i++) {
-        //Get the current hour and subtract for the number of time blocks (so we get the prior hours)
-        // var sbtrHours = moment(testTime, "ha").subtract(i, 'hours').format("ha");
-        // // var addHours = moment(testTime, "ha").add(i + 1, 'hours').format("ha");
-
-        // // const start = moment().hour("8am", "ha");
-        // // const end = moment().hour("6pm", "ha");
-
-        var testFormat = moment(timeBlockEls[i].textContent, "ha").format("HH");
-        console.log()
-        hoursArray.push(testFormat);
-
-        // var test = moment().hour(testTime).format("ha");
-
-        // var testFormat = moment(timeBlockEls[i].textContent, "ha").format("ha");
-
-        // if (testFormat.isBefore(currentHour)) {
-        //     priorHoursArr.push(timeBlockEls[i].textContent);
-        // }
-        // // if (timeBlockEls[i].textContent == currentHour) {
-        //     console.log("Equal!");
-        // }
-        // if (timeBlockEls[i].textContent > testTime) {
-        //     futureHoursArr.push(timeBlockEls[i].textContent);
-        // }
-
-        // if (moment(sbtrHours, "ha").isBetween(start, end, 'hours')) {
-        //     priorHoursArr.push(sbtrHours);
-        //     // if (currentHour.isBetween(start, end, 'hours')) {
-        //     // }
-        // }
-        // if (moment(sbtrHours, "ha").isBefore(start)) {
-        //     console.log("Before");
-        // }
-        // if (moment(sbtrHours, "ha").isAfter(start)) {
-        //     console.log("aFTER");
-        // }
-
-
-
-        // Do something for hours before 9am
-        // else if (moment(sbtrHours, "ha").isAfter(end)) {
-        //     console.log("After");
-        // }
-
-        // else if (moment(sbtrHours).isBefore(start, 'hours')) {
-        //     console.log("Out of scope!");
-        // }
-        // else if (sbtrHours.isAfter(end, 'hours')) {
-        //     console.log("Out of scope!");
-        // }
-
-
-
-        //Push to a new array to check
-
-        // console.log(addHours);
-        // // console.log(testTime)
-        // console.log(priorHoursArr);
-        // console.log(futureHoursArr);
-
-        //Create an object containing the previous and future hours array to return
-        // var datesObj = {
-        //     priorHours: priorHoursArr,
-        //     futureHours: futureHoursArr
-        // };
-
-        // return datesObj;
+        var formattedHours = moment(timeBlockEls[i].textContent, "ha").format("HH");
+        hoursArray.push(formattedHours);
     }
-    // console.log("prior: " + priorHoursArr);
-    // console.log("Other" + futureHoursArr);
-
-
-
     return hoursArray;
 }
 
@@ -99,63 +28,72 @@ let handleDates = function () {
     //Reference our returned array
     var plannerHours = pushDates()
     //Test time
-    const currentTimeTest = moment().hours(10).format("HH");
+    const currentTimeTest = moment().hours(15).format("HH");
     var priorHoursArr = [];
     var futureHoursArr = [];
-    var currentHour = [];
+    var currentHour;
 
-    console.log(plannerHours);
-
-    // console.log("Dates: " + dates);
+    const timeBlockEls = $(".time-block")
 
     for (var i = 0; i < plannerHours.length; i++) {
+        //Ensure the hour is formatted correctly
         var hourIndex = moment(plannerHours[i], "HH").format("HH");
 
-        console.log(plannerHours);
-        console.log(hourIndex);
-
-        //Check the values which indicate the current hour
+        //Check the values which indicate the current hour and use index to style the elements
         if (parseInt(hourIndex) < parseInt(currentTimeTest)) {
             priorHoursArr.push(hourIndex);
+            $(timeBlockEls[i]).next().addClass("past");
         }
         if (parseInt(hourIndex) > parseInt(currentTimeTest)) {
             futureHoursArr.push(hourIndex);
+            $(timeBlockEls[i]).next().addClass("future");
         }
         if (parseInt(hourIndex) === parseInt(currentTimeTest)) {
-            currentHour.push(hourIndex);
+            currentHour = hourIndex;
+            $(timeBlockEls[i]).next().addClass("present");
         }
     }
 
-    console.log(priorHoursArr);
-    console.log(futureHoursArr);
-    console.log(currentHour);
+    //     //Store 
+    //     var hoursObj = {
+    //         priorHours: priorHoursArr,
+    //         futureHours: futureHoursArr,
+    //         currentHour: currentHour
+    //     }
 
-    // const currentHour = moment().format("ha");
-    // const timeBlockEls = $(".time-block");
-    // // console.log(currentTime);
-    // var timeArray = [];
-    // //Iterate through each of the time blocks to check the current hour
-    // for (var i = 0; i < timeBlockEls.length; i++) {
-    //     timeArray.push(timeBlockEls[i].textContent);
-    //     // // console.log(timeBlockEls[i]);
-    //     // if (timeBlockEls[i].textContent.isBefore(currentHour)) {
-    //     //     $(timeBlockEls[i]).next().addClass("past");
-    //     //     // console.log("Current: " + timeBlockEls[i].textContent);
-    //     // }
-
-    //     // if (timeBlockEls[i].textContent === currentHour) {
-    //     //     //Applies the style to the next td (being the description parent)
-    //     //     $(timeBlockEls[i]).next().addClass("present");
-    //     //     console.log("Current: " + timeBlockEls[i].textContent);
-    //     // }
-    //     // else {
-    //     //     $(timeBlockEls[i]).next().addClass("past");
-    //     //     console.log("Other: " + timeBlockEls[i].textContent);
-    //     // }
-    //     // //TODO: Filter out hours prior and future hours
+    //     return hoursObj;
     // }
+
+    // let setTimeBlockColors = function () {
+    //     const timeBlockEls = $(".time-block")
+    //     const timeBlockElTxt = $(".time-block").text();
+    //     var datesObj = handleDates();
+    //     // console.log(datesObj);
+
+    //     for (var i = 0; i < datesObj.priorHours.length; i++) {
+    //         var priorHoursHa = moment(datesObj.priorHours[i], "HH").format("HH");
+    //         console.log(priorHoursHa);
+
+    //         var formattedTextBlock = moment(timeBlockEls[i], "ha").format("HH");
+
+    //         console.log(formattedTextBlock);
+
+    //         // Check if current hour
+    //         if (formattedTextBlock === datesObj.currentHour) {
+    //             console.log("WOOO");
+    //         }
+
+
+    //     }
+    // }
+
 }
 
 handleDates();
 
-// console.log(timeArray);
+// setTimeBlockColors();
+
+
+
+
+
